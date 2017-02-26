c2ff9e97eee77bbab34695dd3519ae790d285d5e
luis-garibay <luis26garibay@gmail.com>
2017-02-26 00:36:31 -0600

    added a lil' documentation

--- ViDSortGUI.java
+++ ViDSortGUI.java
@@ -1,13 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * // Usage:
+ * ViDSortGUI gui = new ViDSortGUI("ViDSort");
+ *
+ * ...
+ * 
+ * gui.updateArray(arr); // draw representation of array
  */
 
-/**
- *
- * @author luis
- */
 public class ViDSortGUI {
     private Window window;
     private UserControlMenu userControlMenu = new UserControlMenu();

e58b70cb05af391cfff4e515da5c0ed412edc081
luis-garibay <luis26garibay@gmail.com>
2017-02-25 23:55:54 -0600

    created simple GUI - L

--- UserControlMenu.java
+++ UserControlMenu.java
@@ -1,33 +1,28 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Window extends JFrame {
-    JPanel windowContent;
-    
-    public Window(int width, int height, String title) {
-        setTitle(title);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(width, height);
-        setLocationRelativeTo(null);
+public class UserControlMenu extends JPanel {
+    public UserControlMenu() {
+        Dimension menuDimensions = getSize();
+        int width = menuDimensions.width;
+        int height = menuDimensions.height;
+        
+        JButton toggleStartStopButton = new JButton("START");;
+        
         setLayout(new GridBagLayout());
-        setVisible(true);
-    }
-    
-    public void addLeftPanel(JPanel leftPanel) {
+        setBackground(new Color(220, 220, 220));
+        setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.black));
+        
         GridBagConstraints gridConstraints = getDefaultGridConstraints();
         
-        add(leftPanel, gridConstraints);
-    }
-    
-    public void addRightPanel(JPanel rightPanel) {
-        GridBagConstraints gridConstraints = getDefaultGridConstraints();
-        
-        gridConstraints.gridx = 1;
-        gridConstraints.weightx = 100; 
-        add(rightPanel, gridConstraints);
+        gridConstraints.gridy = 4;
+        add(toggleStartStopButton, gridConstraints);
     }
     
     private GridBagConstraints getDefaultGridConstraints() {
@@ -39,7 +34,7 @@ public class Window extends JFrame {
         gridConstraints.gridheight = 1;
         gridConstraints.weightx = 50;
         gridConstraints.weighty = 100;
-        gridConstraints.insets = new Insets(0, 0, 0, 0);
+        gridConstraints.insets = new Insets(5, 5, 5, 5);
         gridConstraints.anchor = GridBagConstraints.CENTER;
         gridConstraints.fill = GridBagConstraints.BOTH;
         

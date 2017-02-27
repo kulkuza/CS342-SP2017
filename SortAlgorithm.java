240210cb2ee180ac1b89c8ff463aa0b87a771101
Michael Mei <michaelfmei1@gmail.com>
2017-02-27 00:05:16 -0600

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -1,5 +1,13 @@
-public abstract class SortAlgorithm 
-{
-
-
+public abstract class SortAlgorithm {
+  private String name;  //name of sorting algorithm
+  
+  public AbstractSortAlgorithm(String name) {
+    this.name = name;
+  }
+  
+  public final String getName() {
+    return name;
+  }
+  
+  public abstract void sort(SortArray array);
 }

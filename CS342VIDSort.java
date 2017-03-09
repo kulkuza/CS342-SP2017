35641466dc66018b414db0daddf40d3763c98573
luis-garibay <luis26garibay@gmail.com>
2017-03-09 00:44:17 -0600

    updated example code - L

--- CS342VIDSort.java
+++ CS342VIDSort.java
@@ -12,6 +12,6 @@ public class CS342VIDSort
 		 */
 		int[] testArray = {37, 6, 21, 17, 30, 2, 14, 40, 9, 32, 39, 15};
 		ViDSortGUI gui = ViDSortGUI.getInstance("ViDSort");
-		gui.updateVisualArray(testArray); // display the array
+		gui.update(testArray); // display the array
 	}
 }

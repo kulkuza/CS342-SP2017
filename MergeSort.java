f0df0b578322b25265bfffa20ff9f3cb44caf4c9
jmei32 <jmei1196@gmail.com>
2017-03-12 17:12:04 -0500

    Skeleton for MergeSort

---
+++ MergeSort.java
@@ -0,0 +1,16 @@
+public class MergeSort extends SortAlgorithm {
+	public MergeSort() {
+		super("MergeSort");
+	}
+	
+	public void sort(SortArray array){
+		int size = array.getSize();
+		int midPoint = size/2;
+		int leftSize = midPoint;
+		int rightSize = size - midPoint;
+	}
+	
+	public void merge(){
+		
+	}
+}

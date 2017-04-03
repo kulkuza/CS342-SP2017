74f9994600e44788f5e19b9e71b624d687bd0109
luis-garibay <luis26garibay@gmail.com>
2017-04-02 21:53:27 -0500

    set gui defaults in main() - L

--- CS342VIDSort.java
+++ CS342VIDSort.java
@@ -24,6 +24,7 @@ public class CS342VIDSort
 	public static void main (String[] args)
 	{
 		ViDSortGUI gui = ViDSortGUI.getInstance();
+		gui.setDefaultSettings();
         
         Timer tt = new Timer();
         tt.schedule(new TimerTask() {

ac89520c1f192f9af4d48f64b3afcaa1801f4c35
Michael <michaelfmei1@gmail.com>
2017-04-16 15:20:55 -0500

    Compare is now functioning. Added multithreading for compare

--- CS342VIDSort.java
+++ CS342VIDSort.java
@@ -2,23 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-
-/*
- *	TODO: -fully implement textbox for array creation 
- *			and dropdown menu for algorithm selection
- *		  -Send data from the VisualArray to the GUI to get updated
- *
- *	Steps when start button is pressed:
- *		1) The size is saved from the textbox as an int.
- *		2) An instance of SortAlgorithm is created depending on dropmenu selection
- *			a) For example, if bubblesort was selected, then
- *				SortAlgorithm algorithm = new BubbleSort();
- *		3) An instance of the SortArray is created with the info from 1) and 2)
- *			a) For example,
- *				SortArray newArray = new VisualArray(size, algorithm)
- *
- */
-
 public class CS342VIDSort
 {
 	public static void main (String[] args)
@@ -32,9 +15,37 @@ public class CS342VIDSort
             public void run() {
                 if (gui.isRunningSort()) {
                     int size = gui.getSelectedSize();
-                    SortArray testVisualArray = new VisualArray(size, gui.getSelectedRightAlgorithm(), true);
-                    
-                    testVisualArray.sort();
+                    if (gui.getMode() == ViDSortGUI.Mode.COMPARISON_MODE) {     //create 2 threads to run both sorts simultaneously
+                        Thread threadRight = new Thread() {
+                            public void run() {
+                                SortArray testVisualArray = new VisualArray(size, gui.getSelectedRightAlgorithm(), true);
+                                testVisualArray.sort();
+                            }
+                        };
+
+                        Thread threadLeft = new Thread() {
+                            public void run() {
+                                SortArray testVisualArray = new VisualArray(size, gui.getSelectedLeftAlgorithm(), false);
+                                testVisualArray.sort();
+                            }
+                        };
+                        
+                        threadRight.start();
+                        threadLeft.start();
+
+                        try {
+                            threadRight.join();
+                            threadLeft.join();
+                        }
+                        catch (InterruptedException e) {
+                            System.exit(-1);
+                        }
+                        
+                    }
+                    else {
+                        SortArray testVisualArray = new VisualArray(size, gui.getSelectedRightAlgorithm(), true);
+                        testVisualArray.sort();
+                    }
                     
                     gui.toggleRunningSort();
                 }
